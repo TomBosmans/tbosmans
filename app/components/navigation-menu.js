099012e43919075371e6415d30b3edd17a1d31e2
TomBosmans <t.bosse@hotmail.com>
2018-07-18 18:47:34 +0200

    basic layout

---
+++ app/components/navigation-menu.js
@@ -0,0 +1,6 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  title: 'Tom Bosmans',
+  logo: 'assets/logos/ruby-logo.svg'
+});
---
+++ app/components/page-header.js
@@ -0,0 +1,9 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  title: 'Tom Bosmans',
+  logo: 'assets/logos/ruby-logo.svg',
+  description: 'Web Developer, Gamer and Metalhead',
+  subDescription: 'also ruby lover',
+  btnMessage: 'Welcome!'
+});
